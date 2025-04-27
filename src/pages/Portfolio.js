43de61056dad70c30ad368d8fe0b0c637e184e8a
Sanam-Chaudhary-Code <sanamchaudhary344@gmail.com>
2025-04-27 15:43:03 +0545

    add video call image and update potfolio.js fiel

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,9 +3,18 @@ import '../styles/Portfolio.css';
 import PortfolioImage from '../assets/project-image.png';
 import TicImage from '../assets/tic-tak-toc.jpg';
 import SimonSaysImage from '../assets/simon-says-image.jpg';
+import MyVideoCall from '../assets/my-video-call-image.png'; 
 
 // Sample data for your projects
 const projects = [
+  {
+    title: 'My Video Call',
+    description: 'Real-time communication app with chat, screen sharing, and audio/video calls.',
+    link: 'https://my-video-call-app-jduv.onrender.com/',
+    technologies: ['HTML', 'CSS', 'React', 'Node.js', 'MongoDB'],
+    image: MyVideoCall,
+  }
+  ,
   {
     title: 'Tic-Tac-Toe',
     description: 'A 3x3 grid game with alternating player moves.',
@@ -36,8 +45,8 @@ const Portfolio = () => {
   const allTechnologies = ['All', ...new Set(projects.flatMap(p => p.technologies))];
 
   // Filter projects
-  const filteredProjects = selectedTech === 'All' 
-    ? projects 
+  const filteredProjects = selectedTech === 'All'
+    ? projects
     : projects.filter(project => project.technologies.includes(selectedTech));
 
   return (
@@ -49,9 +58,9 @@ const Portfolio = () => {
       {/* Navigation Bar */}
       <nav className="portfolio-nav">
         {allTechnologies.map((tech, index) => (
-          <button 
-            key={index} 
-            className={selectedTech === tech ? 'active' : ''} 
+          <button
+            key={index}
+            className={selectedTech === tech ? 'active' : ''}
             onClick={() => setSelectedTech(tech)}
           >
             {tech}
